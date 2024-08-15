997da3018ff04ca5bd67cd87d4c8a7e936004ce7
Eugene Sytnyk <e.sytnyk@autodoc.eu>
2024-08-15 19:25:08 +0300

    Create Application

--- app/src/main/java/eugene/sytnyk/testtask/activity/MainActivity.kt
+++ app/src/main/java/eugene/sytnyk/testtask/activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package eugene.sytnyk.testtask
+package eugene.sytnyk.testtask.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import eugene.sytnyk.testtask.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/eugene/sytnyk/testtask/application/AppApplication.kt
@@ -0,0 +1,5 @@
+package eugene.sytnyk.testtask.application
+
+import android.app.Application
+
+class AppApplication : Application()
