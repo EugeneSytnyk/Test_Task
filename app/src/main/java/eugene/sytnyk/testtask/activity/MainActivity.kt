ca4bcb1acd6464da30daad5b58a0fb749631f5cb
Eugene Sytnyk <e.sytnyk@autodoc.eu>
2024-08-15 19:42:45 +0300

    Add ViewModel

--- app/src/main/java/eugene/sytnyk/testtask/activity/MainActivity.kt
+++ app/src/main/java/eugene/sytnyk/testtask/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package eugene.sytnyk.testtask.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,6 +11,9 @@ import eugene.sytnyk.testtask.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
