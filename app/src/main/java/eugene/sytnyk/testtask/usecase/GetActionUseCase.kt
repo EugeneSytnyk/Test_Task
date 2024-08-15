feb67a0d17af3a30f1a004f0360620d8f80fd96a
Eugene Sytnyk <e.sytnyk@autodoc.eu>
2024-08-15 21:42:33 +0300

    Add network state checks

--- app/src/main/java/eugene/sytnyk/testtask/usecase/GetActionUseCase.kt
+++ app/src/main/java/eugene/sytnyk/testtask/usecase/GetActionUseCase.kt
@@ -3,21 +3,28 @@ package eugene.sytnyk.testtask.usecase
 import android.icu.util.Calendar
 import eugene.sytnyk.testtask.mapper.mapTo
 import eugene.sytnyk.testtask.model.ActionUI
+import eugene.sytnyk.testtask.network.NetworkStateManager
 import eugene.sytnyk.testtask.repository.ActionDataRepository
 import eugene.sytnyk.testtask.repository.ActionUsageTimeRepository
 import javax.inject.Inject
 
 class GetActionUseCase @Inject constructor(
     private val actionDataRepository: ActionDataRepository,
-    private val actionUsageTimeRepository: ActionUsageTimeRepository
+    private val actionUsageTimeRepository: ActionUsageTimeRepository,
+    private val networkStateManager: NetworkStateManager
 ) {
+
+    companion object {
+        private val CHECK_CONNECTION_TYPE = setOf("toast")
+    }
 
     suspend fun getAction(): ActionUI? {
         val actions = actionDataRepository.getActionData().getOrNull() ?: return null
         val selectedAction = actions.filter { it.enabled }
             .sortedByDescending { it.priority }
             .filter { dataMeetsCondition(it.validDays) }
-            .firstOrNull { lastUsageTimeMeetsCondition(it.type, it.coolDown) } ?: return null
+            .filter { lastUsageTimeMeetsCondition(it.type, it.coolDown) }
+            .firstOrNull { internetConnectionMeetsCondition(it.type) } ?: return null
 
         val currentTime = System.currentTimeMillis()
         actionUsageTimeRepository.setLastUsageTimeForActionType(selectedAction.type, currentTime)
@@ -26,7 +33,7 @@ class GetActionUseCase @Inject constructor(
     }
 
     private fun dataMeetsCondition(availableDates: List<Int>): Boolean {
-        // Should -1 because calendar returns dates in range 1..7
+        // -1 because calendar returns dates in range 1..7
         val currentDay = Calendar.getInstance().get(Calendar.DAY_OF_WEEK) - 1
         return availableDates.contains(currentDay)
     }
@@ -35,5 +42,10 @@ class GetActionUseCase @Inject constructor(
         val currentTime = System.currentTimeMillis()
         val lastUsage = actionUsageTimeRepository.getLastUsageTimeForActionType(type) ?: return true
         return currentTime - lastUsage < coolDown
+    }
+
+    private fun internetConnectionMeetsCondition(type: String): Boolean {
+        if (type !in CHECK_CONNECTION_TYPE) return true
+        return networkStateManager.hasInternetConnection()
     }
 }
